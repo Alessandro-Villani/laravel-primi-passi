1aced80c0c5e3a7f2f4d1e6d60ae2779151bc52c
Alessandro-Villani <alessvill@libero.it>
2023-02-22 16:40:54 +0100

    added more pages and route - end

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- BOOTSTRAP --}}
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
-    <title>Hello Laravel</title>
+    <title>Document</title>
 </head>
 <body class="bg-secondary">
     <header class="bg-dark py-5 text-white">
@@ -19,7 +19,7 @@
         </nav>
     </header>
     <main class="text-center text-white py-5">
-        <h1>{{$name}}</h1>
+        <h1>{{ucfirst(str_replace('-', ' ', $name))}}</h1>
     </main>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $menus = ['home', 'contatti', 'about us'];
-    return view('home', compact('menus'));
+    $name = 'Home';
+    $menus = ['home', 'contatti', 'about-us'];
+    return view('home', compact('menus', 'name'));
 })->name('home');
+
+Route::get('/contacts/{index}', function ($index) {
+    $menus = ['home', 'contatti', 'about-us'];
+    $name = $menus[$index];
+    return view('contacts', compact('menus', 'name'));
+})->name('contatti');
+
+Route::get('/about-us/{index}', function ($index) {
+    $menus = ['home', 'contatti', 'about-us'];
+    $name = $menus[$index];
+    return view('about-us', compact('menus', 'name'));
+})->name('about-us');
